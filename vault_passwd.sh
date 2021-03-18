794f1f802b0491e158a06c62a0dce91af68ff815
Etienne Filliat <etienne.filliat@epitech.eu>
2021-03-18 12:06:47 +0100

    Add password prompt for vault_passwd

--- vault_passwd.sh
+++ vault_passwd.sh
@@ -5,5 +5,7 @@ if [ -f vault_passwd.clear ]; then
 elif [ -f vault_passwd.gpg ]; then
   gpg --decrypt vault_passwd.gpg 2>/dev/null
 else
-  echo "Error: vault password should be stored in vault_password.clear, or encrypted with GPG in vault_passwd.gpg!" >&2
+  read -s -p "Password: " password
+  echo $password
+  #echo "Error: vault password should be stored in vault_password.clear, or encrypted with GPG in vault_passwd.gpg!" >&2
 fi
